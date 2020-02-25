383e1a33f88de9c7f758d69acb8fbf8cbd40b2e3
phil cowan <philipjcowan1@gmail.com>
2020-02-25 10:55:17 -0700

    created database and saved sql code in products.sql

---
+++ products.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INTEGER(10) NOT NULL,
+  PRIMARY KEY (id)
+  );
+  
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Macbook Pro", "Electronics", 1200.00, 10),
+("SmartTV", "Electronics", 800.00, 30),
+("Gourmet Cutlery Set", "Kitchen", 149.99, 25),
+("Memory Foam Dog Bed", "Pet Supplies", 24.99, 15),
+("Reusable Metal Straws 4pk", "Kitchen",  9.99, 100),
+("Random Access Memories Record", "Vinyl", 23.99, 12),
+("No Pull Dog Harness", "Pet Supplies", 13.99, 20),
+("Gaggia Brera Espresso Machine", "Appliances", 449.99, 33),
+("Kind of Blue Original Pressing", "Vinyl", 159.99, 1),
+("Saffire Pro40 Interface", "Recording Studio", 450.00, 15),
+("Neumann U87 Large-diaphragm Condenser Mic", "Recording Studio", 3600.00, 3);
+
+SELECT * FROM products;
+
